e279294770d6269215834001792b95934b791e5f
vudinhquang <vudinhquangk53@gmail.com>
2019-07-06 16:18:52 +0900

    Confirm - Màn hình confirm khi người dùng vào email để click vào link đăng nhập

--- confirm_login.php
+++ confirm_login.php
@@ -2,6 +2,26 @@
 
 include_once 'autoload.php';
 
-echo "<pre>";
-print_r($_GET);
-echo "</pre>";
+if (!empty(Session::get('email'))) URL::redirect('setting.php');
+
+$code = isset($_GET['code']) ? $_GET['code'] : '';
+$time = isset($_GET['time']) ? $_GET['time'] : '';
+$email = isset($_GET['email']) ? $_GET['email'] : '';
+
+if ($time + EXPIRED_TIME < time()) {
+  echo '<h3>Link hết hạn!! Vui lòng đăng nhập lại <a href="login.php">tại đây</a></h3>';
+} else {
+  $data = json_decode(file_get_contents(DATA_USER), TRUE);
+  $userInfo = $data[$email];
+
+  if ($userInfo['login_time'] + EXPIRED_TIME < time()) {
+    echo '<h3>Link hết hạn!! Vui lòng đăng nhập lại <a href="login.php">tại đây</a></h3>';
+  } else {
+    if ($userInfo['login_key'] === $code) {
+      Session::set('email', $email);
+      URL::redirect('setting.php');
+    } else {
+      echo '<h3>Thất bại!! Vui lòng đăng nhập lại <a href="login.php">tại đây</a></h3>';
+    }
+  }
+}
--- define.php
+++ define.php
@@ -4,5 +4,6 @@
     define('PATH_LIBS'	, PATH_ROOT . DS . 'libs' . DS);
     define('PATH_DATA'	, PATH_ROOT . DS . 'data' . DS);
     define('DATA_USER'  , PATH_DATA . 'users.json');
-    define('SECRET_KEY' , 'Abdu!@#123');	
+    define('SECRET_KEY' , 'Abdu!@#123');
+    define('EXPIRED_TIME' , 60);		
 ?>
